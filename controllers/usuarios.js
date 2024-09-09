b50d34bdad8f3b31a5460601fd2f32d7a94d307c
TheusMarques <135022237+TheusMarques@users.noreply.github.com>
2024-09-09 09:10:01 -0300

    ojbnpiou

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -18,5 +18,69 @@ module.exports = {
             });
         }
     }
+    async ListarUsuario(request, response){
+        try{
+            //throw new Error('Eu causei o erro!');
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Lista de Usuarios',
+                dados: null
+            });
+        } catch (error){
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na Requisição',
+                dados: error.message
+            });
+        }
+    }
+    async ListarUsuario(request, response){
+        try{
+            //throw new Error('Eu causei o erro!');
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Lista de Usuarios',
+                dados: null
+            });
+        } catch (error){
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na Requisição',
+                dados: error.message
+            });
+        }
+    }
+    async ListarUsuario(request, response){
+        try{
+            //throw new Error('Eu causei o erro!');
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Lista de Usuarios',
+                dados: null
+            });
+        } catch (error){
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na Requisição',
+                dados: error.message
+            });
+        }
+    }
+    async ListarUsuario(request, response){
+        try{
+            //throw new Error('Eu causei o erro!');
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Lista de Usuarios',
+                dados: null
+            });
+        } catch (error){
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na Requisição',
+                dados: error.message
+            });
+        }
+    }
 }
 
